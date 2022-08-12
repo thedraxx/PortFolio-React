5cfcee78d2feeb5693d40b3d599837b7fd6a8855
Frank <55867157+thedraxx@users.noreply.github.com>
2022-08-12 11:55:45 -0300

    delete animatecss

--- src/routes/Rutas.jsx
+++ src/routes/Rutas.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { Routes, Route } from 'react-router-dom'
+import Experience from '../pages/Home/experience/Experience'
 import Home from '../pages/Home/Home'
 
 export const Rutas = () => {
