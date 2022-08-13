8e9851b45395a9a8a15aaf1472009b1192419331
Frank <55867157+thedraxx@users.noreply.github.com>
2022-08-13 17:05:12 -0300

    connect emailJS

--- src/components/form/FormSubmit.jsx
+++ src/components/form/FormSubmit.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useSelector } from 'react-redux'
 import { useForm } from '../../hooks/useForm'
+import emailjs from '@emailjs/browser'
 import {
   ButtonSubmit,
   ContainerForms,
@@ -10,6 +11,7 @@ import {
 } from './style'
 
 export const FormSubmit = () => {
+  const form = useRef()
   const { active } = useSelector((state) => state.isActive)
 
   const [formValues, handleInputChange] = useForm({
@@ -20,16 +22,21 @@ export const FormSubmit = () => {
 
   const { searchText } = formValues
 
-  const handleSearch = (e) => {
+  const sendEmail = (e) => {
     e.preventDefault()
-    console.log(formValues)
+    emailjs.sendForm('service_qksi4tc', 'template_zqzc909', form.current, 'ywb4feFd4jk6dBlo8')
+      .then((result) => {
+        alert('Mensaje enviado correctamente')
+      }, (error) => {
+        console.log(error.text)
+      })
   }
 
   return (
     <ContainerGeneral>
       <ContainerInputs>
         <ContainerForms>
-          <form onSubmit={handleSearch}>
+          <form onSubmit={sendEmail} ref={form}>
             <CustomInput
               active={active}
               type="text"
