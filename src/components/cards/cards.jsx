8cce6d689cc3a549d14d3b587d0a4f7f793bdbea
Frank <55867157+thedraxx@users.noreply.github.com>
2022-08-13 21:31:29 -0300

    update

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Card from 'react-bootstrap/Card'
 import { useSelector } from 'react-redux'
 import { CardLink, ContainerLinks, CustomDiv, ImageCard, TitleCard, TittleDescription } from './style'
-import { Link } from 'react-router-dom'
 
 const ProyectTemplate = ({ project }) => {
   const { active } = useSelector((state) => state.isActive)
